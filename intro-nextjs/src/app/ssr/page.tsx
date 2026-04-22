7fd803b6157505851d14fcfa99abe017a82edcae
masdefry <muhammaddefryan@gmail.com>
2026-04-22 21:13:54 +0700

    ssr & csr

--- intro-nextjs/src/app/ssr/page.tsx
+++ intro-nextjs/src/app/ssr/page.tsx
@@ -1,7 +1,23 @@
-export default function Page(){
-    return(
-        <>
-            <h1>SSR PAGE</h1>
-        </>
-    )
+export const getPostsData = async () => {
+  try {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts'); // Format response is JSON
+    console.log(res);
+    const posts = await res.json(); // Convert JSON tobe object JS
+
+    return posts;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default async function Page() {
+  const postsData = await getPostsData();
+  return (
+    <>
+      <h1>SSR PAGE</h1>
+      {postsData?.map((post: any, index: number) => {
+        return <h2 key={index}>{post?.title}</h2>;
+      })}
+    </>
+  );
 }
