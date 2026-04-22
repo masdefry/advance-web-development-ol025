82ad7b27db2280b56eb7c5e65618df54ff26a124
masdefry <muhammaddefryan@gmail.com>
2026-04-22 20:48:40 +0700

    intro next.js

---
+++ intro-nextjs/next.config.ts
@@ -0,0 +1,17 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.hellosehat.com',
+        port: '',
+        pathname: '/wp-content/uploads/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
