0ba8849b016205c56752887cf2c945a79028e7c6
M Defryan <muhammaddefryan@gmail.com>
2026-05-11 19:44:19 +0700

    day05: upload file handling frontend

--- pos-app/src/app/dashboard/menus-management/create/page.tsx
+++ pos-app/src/app/dashboard/menus-management/create/page.tsx
@@ -1,45 +1,87 @@
-export default function CreateNewMenuPage(){
-    return(
-        <>
-            <h2 className="text-2xl font-bold mb-4">Create New Menu</h2>
-            <form>
-                <fieldset className="fieldset">
-                    <legend className="fieldset-legend">Menu Name</legend>
-                    <input type="text" className="input w-full" placeholder="Type here" />
-                    <p className="label">Optional</p>
-                </fieldset>
+'use client';
+import { useFormik } from 'formik';
 
-                <fieldset className="fieldset">
-                    <legend className="fieldset-legend">Price</legend>
-                    <input type="text" className="input w-full" placeholder="Type here" />
-                    <p className="label">Optional</p>
-                </fieldset>
+export default function CreateNewMenuPage() {
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      price: 0,
+      isAvailable: false,
+      files: [] as File[],
+    },
+    onSubmit: ({ name, price, isAvailable, files }: any) => {
+      console.log(name);
+      console.log(price);
+      console.log(isAvailable);
+      console.log(files);
+    },
+  });
 
-                <fieldset className="fieldset">
-                    <legend className="fieldset-legend">Description</legend>
-                    <textarea className="textarea h-24 w-full" placeholder="Bio"></textarea>
-                    <div className="label">Optional</div>
-                </fieldset>
+  return (
+    <>
+      <h2 className='text-2xl font-bold mb-4'>Create New Menu</h2>
+      <form onSubmit={formik?.handleSubmit}>
+        <fieldset className='fieldset'>
+          <legend className='fieldset-legend'>Menu Name</legend>
+          <input
+            type='text'
+            name='name'
+            onChange={formik?.handleChange}
+            className='input w-full'
+            placeholder='Type here'
+          />
+          <p className='label'>Optional</p>
+        </fieldset>
 
-                <fieldset className="fieldset">
-                    <legend className="fieldset-legend">Menu Image(s)</legend>
-                    <input type="file" className="file-input w-full" />
-                    <label className="label">Max size 2MB</label>
-                </fieldset>
+        <fieldset className='fieldset'>
+          <legend className='fieldset-legend'>Price</legend>
+          <input
+            type='text'
+            name='price'
+            onChange={formik?.handleChange}
+            className='input w-full'
+            placeholder='Type here'
+          />
+          <p className='label'>Optional</p>
+        </fieldset>
 
-                <div className="form-control mb-3">
-                <label className="label cursor-pointer justify-start gap-3">
-                    <input type="checkbox" className="toggle toggle-primary" />
-                    <span className="label-text">Is Available</span>
-                </label>
-                </div>
+        <fieldset className='fieldset'>
+          <legend className='fieldset-legend'>Menu Image(s)</legend>
+          <input
+            type='file'
+            name='files'
+            onChange={(e) => {
+              if (e.currentTarget.files) {
+                formik?.setFieldValue(
+                  'files',
+                  Array.from(e.currentTarget.files),
+                );
+              }
+            }}
+            className='file-input w-full'
+            multiple
+          />
+          <label className='label'>Max size 2MB</label>
+        </fieldset>
 
-                <div className="form-control">
-                <button type="submit" className="btn btn-primary w-full">
-                    Save Menu
-                </button>
-                </div>
-            </form>
-        </>
-    )
+        <div className='form-control mb-3'>
+          <label className='label cursor-pointer justify-start gap-3'>
+            <input
+              type='checkbox'
+              name='isAvailable'
+              onChange={formik?.handleChange}
+              className='toggle toggle-primary'
+            />
+            <span className='label-text'>Is Available</span>
+          </label>
+        </div>
+
+        <div className='form-control'>
+          <button type='submit' className='btn btn-primary w-full'>
+            Save Menu
+          </button>
+        </div>
+      </form>
+    </>
+  );
 }
