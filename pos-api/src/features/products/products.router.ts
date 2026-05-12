78278220b583e2c2ced61dc20e25a4e3b5cfbf97
M Defryan <muhammaddefryan@gmail.com>
2026-05-12 19:48:54 +0700

    day05: cloudinary upload

--- pos-api/src/features/products/products.router.ts
+++ pos-api/src/features/products/products.router.ts
@@ -1,11 +1,14 @@
 import { productsController } from './products.controller';
 import { Router } from 'express';
 import { multerUploads } from '../../lib/multer.lib';
+import { authMiddleware } from '../../middlewares/auth.middleware';
 
 export const productsRouter = Router();
 
 productsRouter.post(
   '/',
+  authMiddleware.verifyToken, 
+  authMiddleware.requireAuth(['ADMIN']),
   multerUploads.uploads(['jpg', 'jpeg', 'png', 'webp', 'svg']).fields([
     {
       name: 'productImages',
